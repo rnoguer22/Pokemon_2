9a9e4a86d4e55f034373eded75c4b8291d04f44e
rnoguer22 <rnogugon@myuax.com>
2022-04-28 09:33:44 +0200

    Calculamos la media del ataque y la defensa

--- Analisis.py
+++ Analisis.py
@@ -15,7 +15,36 @@ class Grafico:
         plt.xlabel(None)
         plt.show()
 
+class Media(Grafico):
+    #Constructor
+    def __init__(self, dataset, col1, col2):
+        super().__init__(dataset, col1, col2)
+
+    def calcular_media(self):
+
+        def calcular_xini():
+            xini = []
+            for i in range (len(self.dataset)):
+                resultado = self.dataset[self.col1][i]*self.dataset[self.col2][i]
+                xini.append(resultado)
+            return xini
+
+        self.dataset["XiNi"] = calcular_xini()
+        suma_xini = self.dataset["XiNi"].sum()
+        suma_ni = self.dataset[self.col2].sum()
+
+        return suma_xini/suma_ni
+
+
+
+
 if __name__ == "__main__":
 
+    #Hacemos el grafico de barras comparando ataque y defensa
     grafico = Grafico("Pokemon.csv","Attack","Defense")
-    grafico.grafico_barras("Grafico Ataque-Defensa")
+    #grafico.grafico_barras("Grafico Ataque-Defensa")
+
+    #Calculamos la media de ataque y defensa
+    media = Media("Pokemon.csv","Attack","Defense")
+    print ("La media es: {}".format(media.calcular_media()))
+
