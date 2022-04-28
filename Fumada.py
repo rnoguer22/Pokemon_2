881e53d5fb95c587d760e323c6b28eb3368df205
rnoguer22 <rnogugon@myuax.com>
2022-04-28 10:16:27 +0200

    Adaptamos el csv a los csv de la solucion

--- Fumada.py
+++ Fumada.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 dt = pd.read_csv("Pokemon.csv")
 
-eliminar = ["Type 2","Sp. Atk","Sp. Def","Speed","Generation","Legendary"]
+eliminar = ["Type 1","Type 2","Sp. Atk","Sp. Def","Speed","Generation","Legendary","Total"]
 for i in eliminar:
     del dt[i]
 
-dt["Type2"] = "Water"
+dt["weapon"] = "kick"
+
+print (dt)
 
 dt.to_csv("Nuevo Dataset.csv")
